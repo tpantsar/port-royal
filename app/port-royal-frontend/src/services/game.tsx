739ee0875251a8ff6d63300ceae07781c4bedbe9
tpantsar <tomi.pantsar@gmail.com>
2024-12-20 20:20:33 +0200

    Create new App component

--- app/port-royal-frontend/src/services/game.tsx
+++ app/port-royal-frontend/src/services/game.tsx
@@ -3,29 +3,26 @@ import { ApiResponse } from '../types/ApiResponse'
 import { Card } from '../types/Card'
 import { GameStatusInfoSimple } from '../types/GameStatusInfoSimple'
 
-const baseUrl = 'http://localhost:8080/api/game'
-const gameStateUrl = `${baseUrl}/state-simple`
-const drawCardUrl = `${baseUrl}/draw-card`
-const buyCardUrl = `${baseUrl}/buy-card`
-
-const getGameState = async (): Promise<ApiResponse<GameStatusInfoSimple>> => {
-  const response = await axios.get<ApiResponse<GameStatusInfoSimple>>(gameStateUrl)
-  return response.data
-}
-
-const drawCard = async (): Promise<ApiResponse<Card>> => {
-  const response = await axios.post<ApiResponse<Card>>(drawCardUrl)
-  return response.data
-}
-
-const buyCard = async (params: BuyCardParams): Promise<ApiResponse<Card>> => {
-  const response = await axios.post<ApiResponse<Card>>(buyCardUrl, params)
-  return response.data
-}
+const baseUrl = 'api/game'
 
 type BuyCardParams = {
   cardId: number
   playerId: number
+}
+
+const getGameState = async (): Promise<ApiResponse<GameStatusInfoSimple>> => {
+  const response = await axios.get<ApiResponse<GameStatusInfoSimple>>(`${baseUrl}/state-simple`)
+  return response.data
+}
+
+const drawCard = async (): Promise<ApiResponse<Card>> => {
+  const response = await axios.post<ApiResponse<Card>>(`${baseUrl}/draw-card`)
+  return response.data
+}
+
+const buyCard = async (params: BuyCardParams): Promise<ApiResponse<Card>> => {
+  const response = await axios.post<ApiResponse<Card>>(`${baseUrl}/buy-card`, params)
+  return response.data
 }
 
 export default {
