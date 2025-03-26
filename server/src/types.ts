275ab50874d6be4c2732d38f27a23b2d59a24407
tpantsar <tomi.pantsar@gmail.com>
2025-03-26 15:21:45 +0200

    add types for cards

---
+++ server/src/types.ts
@@ -0,0 +1,62 @@
+enum ResearchMode {
+  ANCHOR,
+  CROSS,
+  HOUSE,
+}
+
+enum TaxMode {
+  LOWEST_POINTS,
+  MOST_SWORDS,
+}
+
+enum CharacterAbility {
+  FIVE_CARDS = 'FIVE_CARDS',
+  ONE_CHEAPER = 'ONE_CHEAPER',
+  BOARD_EMPTY = 'BOARD_EMPTY',
+  EXTRA_CARD = 'EXTRA_CARD',
+  EXTRA_COIN_SKIFF = 'EXTRA_COIN_SKIFF',
+  EXTRA_COIN_FLUTE = 'EXTRA_COIN_FLUTE',
+  EXTRA_COIN_FRIGATE = 'EXTRA_COIN_FRIGATE',
+  EXTRA_COIN_GALLEON = 'EXTRA_COIN_GALLEON',
+  EXTRA_COIN_PINANCE = 'EXTRA_COIN_PINANCE',
+
+  // For destroying ships
+  SWORDS = 'SWORDS',
+
+  // For buying research cards
+  HOUSE = 'HOUSE',
+  ANCHOR = 'ANCHOR',
+  CROSS = 'CROSS',
+}
+
+export interface CardBase {
+  id: number
+  name: string
+  displayImage: boolean
+  imageName: string
+}
+
+export interface CharacterCard extends CardBase {
+  victoryPoints: number
+  characterCost: number
+  abilities: CharacterAbility[]
+  type: 'character'
+}
+
+export interface ResearchCard extends CardBase {
+  victoryPoints: number
+  researchMode: ResearchMode
+  coinsAmount: number
+  type: 'research'
+}
+
+export interface ShipCard extends CardBase {
+  shipWeapons: number
+  shipCoins: number
+  type: 'ship'
+}
+
+export interface TaxCard extends CardBase {
+  taxMode: TaxMode
+  type: 'tax'
+}
