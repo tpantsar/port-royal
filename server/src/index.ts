b6e4bfd7e859470060d66ac3752d6e8f843a85d3
tpantsar <tomi.pantsar@gmail.com>
2025-05-07 09:47:19 +0300

    add config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import { PORT } from '#config.js'
 import gameRouter from '#routes/game.js'
 import { initGame } from '#utils/state.js'
 import cors from 'cors'
@@ -10,8 +11,6 @@ dotenv.config()
 const app: Application = express()
 app.use(express.json())
 app.use(cors())
-
-const port = process.env.PORT ?? '3000'
 
 // Initialize the game state
 initGame()
@@ -27,6 +26,6 @@ app.get('/ping', (_req: Request, res: Response) => {
 
 app.use('/api/game', gameRouter)
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
 })
