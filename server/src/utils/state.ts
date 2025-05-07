bd045b5c135fa1725858ab8c4fec4c9bede53253
tpantsar <tomi.pantsar@gmail.com>
2025-05-07 10:43:13 +0300

    Initialize real starting data to game state object

--- server/src/utils/state.ts
+++ server/src/utils/state.ts
@@ -1,46 +1,56 @@
 // In-memory based state management for a game
 // This module handles loading and saving the game state to a runtime variable.
-import { GameStatus, GameStatusEnum, Player } from '#types.js'
+import allCards from '#data/cards.js'
+import { Cards, GameStatus, GameStatusEnum, Player } from '#types.js'
+
+const CARDS: Cards = {
+  primaryPile: allCards,
+  tablePile: [],
+  discardPile: [],
+  researchPile: [],
+}
+
+const initialPlayerCoins = 3
+
+// Assign 3 random cards to each player at the start of the game
+// Shuffle the cards and take the first 3
+const assignInitialPlayerCards = () => {
+  const shuffledCards = [...allCards].sort(() => Math.random() - 0.5)
+  const randomCards = shuffledCards.slice(0, initialPlayerCoins)
+
+  // Remove the assigned cards from the primary pile
+  CARDS.primaryPile = CARDS.primaryPile.filter(
+    (card) => !randomCards.some((assignedCard) => assignedCard.id === card.id),
+  )
+
+  return randomCards
+}
 
 // Initial test players
 const PLAYERS: Player[] = [
   {
     id: 1,
     name: 'Player 1',
-    coins: 0,
     score: 0,
-    cards: [],
+    cards: assignInitialPlayerCards(),
+    coins: initialPlayerCoins,
     abilities: [],
   },
   {
     id: 2,
     name: 'Player 2',
-    coins: 0,
     score: 0,
-    cards: [],
+    cards: assignInitialPlayerCards(),
+    coins: initialPlayerCoins,
     abilities: [],
   },
 ]
 
-const CURRENT_PLAYER: Player = {
-  id: 1,
-  name: 'Player 1',
-  coins: 0,
-  score: 0,
-  cards: [],
-  abilities: [],
-}
-
 // This is the main game state that will be modified throughout the game
 export const gameStatus: GameStatus = {
-  cards: {
-    primaryPile: [],
-    tablePile: [],
-    discardPile: [],
-    researchPile: [],
-  },
+  cards: CARDS,
   players: PLAYERS,
-  currentPlayer: CURRENT_PLAYER,
+  currentPlayer: PLAYERS[0], // The current player is the first player in the list
   duplicateColoredShips: false,
   status: GameStatusEnum.IN_PROGRESS,
 }
