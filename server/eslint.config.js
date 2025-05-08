d5c340f9b30e1785bc342c84199c85c461655152
tpantsar <tomi.pantsar@gmail.com>
2025-05-08 09:59:57 +0300

    fix linting errors

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,23 +1,15 @@
 // @ts-check
 import eslint from '@eslint/js'
 import vitest from '@vitest/eslint-plugin'
-import perfectionist from 'eslint-plugin-perfectionist'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  {
+    ignores: ['**/*.js', 'dist'],
+  },
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    ignores: ['**/*.js', 'dist'],
-    plugins: {
-      perfectionist, // https://perfectionist.dev/guide/getting-started#settings
-    },
-    rules: {
-      'perfectionist/sort-objects': 'off',
-    },
-  },
   {
     languageOptions: {
       parserOptions: {
