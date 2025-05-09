39d051c2497635529f227fa5a69f17858418db7a
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-09 12:53:29 +0300

    Add functionality to purchasing ships and characters

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,5 +1,6 @@
 import gameService from '#services/gameService.js'
-import { ApiResponse, Card, GameStatus, Player } from '#types.js'
+import { ApiResponse, Card, CharacterCard, GameStatus, Player, ShipCard } from '#types.js'
+import { gameStatus } from '#utils/state.js'
 import express, { Request, Response } from 'express'
 
 const router = express.Router()
@@ -87,6 +88,93 @@ router.get('/switch', (_req: Request, res: Response<ApiResponse<Player | null>>)
       errors: null,
     }
 
+    res.status(200).json(response)
+  } catch (error) {
+    const response: ApiResponse<null> = {
+      statusCode: 500,
+      message: 'Failed to switch player',
+      data: null,
+      errors: [error instanceof Error ? error.message : 'Unknown error'],
+    }
+
+    res.status(500).json(response)
+  }
+})
+
+router.post('/buy', (_req: Request, res: Response<ApiResponse<GameStatus | null>>) => {
+  try {
+    const nextPlayer = gameService.switchPlayer()
+
+    const gameStatusObject = gameStatus
+
+    let cardBeingBought: Card | null = null
+
+    // for-loop toimii jostain syystä. array.forEach ei toimi cardBeingBought:in kanssa
+    // sanoo cardBeingBought:in olevan "never"
+    const tablePile = gameStatus.cards.tablePile
+    for (let x = 0; x < tablePile.length; x++) {
+      if (tablePile[x].id === _req.body['cardId']) {
+        cardBeingBought = tablePile[x]
+      }
+    }
+
+    if (cardBeingBought === null) {
+      throw new Error('Card was not found!')
+    }
+
+    switch (cardBeingBought.type) {
+      case 'research':
+      case 'tax':
+        throw new Error('You cannot buy that card!')
+      case 'character':
+        handleCharacterPurchase(cardBeingBought)
+        break
+      case 'ship':
+        handleShipPurchase(cardBeingBought)
+        break
+      default:
+        throw new Error('You cannot buy that card!')
+    }
+
+    function handleCharacterPurchase(card: CharacterCard) {
+      if (card.characterCost > gameStatus.currentPlayer.coins) {
+        throw new Error('Not enough coins!')
+      }
+      const coinAmount = card.characterCost
+
+      // add card to player
+      const addedCardArray = gameStatus.currentPlayer.cards.concat(card)
+      // set the old array with new one
+      gameStatus.currentPlayer.coins -= coinAmount
+      gameStatus.currentPlayer.cards = addedCardArray
+      gameStatus.currentPlayer.abilities = [
+        ...gameStatus.currentPlayer.abilities,
+        ...card.abilities,
+      ]
+      gameStatus.cards.tablePile = tablePile.filter((_card) => _card.id !== card.id)
+    }
+
+    function handleShipPurchase(card: ShipCard) {
+      const coinAmount = card.shipCoins
+      const primaryPile = gameStatus.cards.primaryPile
+      const shuffled = [...primaryPile].sort(() => Math.random() - 0.5)
+
+      // delete as many cards from primaryPile as the coin amount is in ship
+      // e.g. if ship gives 3 coins, remove 3 cards from the deck, because they're given to the player's coin deck
+      const newPrimaryPile = shuffled.slice(coinAmount)
+
+      // set the current deck to the right one
+      gameStatus.cards.primaryPile = newPrimaryPile
+      gameStatus.currentPlayer.coins += coinAmount
+      gameStatus.cards.tablePile = tablePile.filter((_card) => _card.id !== card.id)
+    }
+
+    const response: ApiResponse<GameStatus> = {
+      statusCode: 200,
+      message: 'Player switched successfully',
+      data: gameStatusObject,
+      errors: null,
+    }
     res.status(200).json(response)
   } catch (error) {
     const response: ApiResponse<null> = {
