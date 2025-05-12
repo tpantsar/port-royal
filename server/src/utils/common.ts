fe70efb360c51da02f6da711ddffc6bfb3364c54
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-12 11:15:42 +0300

    Add bought ship card to the discard pile

--- server/src/utils/common.ts
+++ server/src/utils/common.ts
@@ -13,9 +13,11 @@ export function handleShipPurchase(card: ShipCard) {
   // e.g. if ship gives 3 coins, remove 3 cards from the deck, because they're given to the player's coin deck
   const newPrimaryPile = shuffled.slice(coinAmount)
   const newCurrentPlayerCards = shuffled.slice(0, coinAmount)
-
   // set the current deck to the right one
   gameStatus.cards.primaryPile = newPrimaryPile
+
+  //move the bought ship card to the discard pile
+  gameStatus.cards.discardPile = [...gameStatus.cards.discardPile, card]
   currentPlayer.coins += coinAmount
   currentPlayer.cards = currentPlayer.cards.concat(newCurrentPlayerCards)
 
